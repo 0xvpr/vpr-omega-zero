3e5e7a0168e42c32cbb5e9334b5ca61e1449c2c7
0xvpr <malikrb@live.com>
2022-03-12 22:16:48 -0600

    Replaced stdio operations with fstream; Improved implementation of 64 bit PE32+ headers; Added nodiscard to each binary processing function

--- Sources/Util.cpp
+++ Sources/Util.cpp
@@ -1,10 +1,9 @@
 #include "Util.hpp"
 
 void __usage_error(char* argv_0, int error_code) {
-    std::cerr << "Usage Error " << error_code << ": " << [&error_code]() -> std::string {
+    std::cerr << "Usage error " << error_code << ": " << [&error_code]() -> std::string {
                   switch (error_code) {
                       case -1: { return "no arguments supplied."; }
-                      case  1: { return "not a regular file."; }
                       default: { return "unknown error."; }
                   }
               }() << "\n"
